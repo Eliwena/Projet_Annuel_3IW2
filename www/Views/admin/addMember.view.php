4cd787a845d029488a92cd52dd957804fca83ab1
Océane <oceane.renoux@hotmail.fr>
2021-06-08 00:29:51 +0200

    Modif style bouton retour add member back

--- www/Views/admin/addMember.view.php
+++ www/Views/admin/addMember.view.php
@@ -5,8 +5,9 @@
     <div>
 		<?php $form->render() ?>
 
-		<a class="btn_add" rel="stylesheet" type="text/css" onclick="window.history.go(-1); return false;">Retour</a>
 		</br></br>
-	</div>
+        <a class="btn_add" rel="stylesheet" type="text/css" onclick="window.history.go(-1); return false;">Retour</a>
+
+    </div>
 
 </section>

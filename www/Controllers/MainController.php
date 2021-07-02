951bfd4b2b807e5d7489791c6dfa312c45118db4
Anthony ARJONA <mimso@outlook.fr>
2021-07-02 14:00:43 +0200

    [FIX] sitemap

--- www/Controllers/MainController.php
+++ www/Controllers/MainController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Core\AbstractController;
 use App\Core\Framework;
 use App\Core\Helpers;
+use App\Core\Router;
 use App\Core\View;
 use App\Core\Database;
 use App\Services\User\Security;
@@ -31,6 +32,37 @@ class MainController extends AbstractController
 		//Affiche la vue 404 intégrée dans le template du front
 		$view = new View("404"); 
 	}
+
+	public function sitemapAction() {
+        header("Content-Type: application/xml; charset=utf-8");
+
+        $routes_exclude = [
+           'app_login_oauth',
+           'app_logout',
+           'app_sitemap',
+       ];
+
+	   $routes = Router::getListOfRoutes();
+
+
+	   $sitemap_content = '';
+	   foreach ($routes as $route_params) {
+            if(!in_array($route_params['name'], $routes_exclude) && !strpos($route_params['name'], 'admin')) {
+                $sitemap_content .= '<url>' . PHP_EOL;
+                $sitemap_content .= '<loc>' . Framework::getUrl($route_params['name']) . '</loc>' . PHP_EOL;
+                $sitemap_content .= '<changefreq>daily</changefreq>' . PHP_EOL;
+                $sitemap_content .= '</url>' . PHP_EOL;
+            }
+       }
+
+        echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+        echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.PHP_EOL;
+        echo $sitemap_content;
+        echo '</urlset>' . PHP_EOL;
+
+
+
+    }
 	
 
 

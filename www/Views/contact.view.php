988d030449323d602787f7baf1daa9d9a04b94c1
Nils <55735148+NilsMillot@users.noreply.github.com>
2021-07-23 09:50:46 +0200

    [ADD] contact page

---
+++ www/Views/contact.view.php
@@ -0,0 +1,16 @@
+<?php
+use App\Core\Framework;
+?>
+<section class="section" style="padding: 2rem">
+   <form method="post">
+       <h1>Contactez-nous!</h1>
+       <input type="text">
+       <button type="submit">Envoyer</button>
+   </form>
+</section>
+<style type="text/css">
+
+    @media (max-width: 890px) {
+
+    }
+</style>

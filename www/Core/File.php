cc6600a6c25476b3aa119e1a2f77d15b9d911d0e
Anthony ARJONA <mimso@outlook.fr>
2021-07-25 03:48:52 +0200

    [ADD] format bytes

--- www/Core/File.php
+++ www/Core/File.php
@@ -101,4 +101,12 @@ class File
         return false;
     }
 
+    public function formatBytes($size, $precision = 2)
+    {
+        $base = log($size, 1024);
+        $suffixes = array('o', 'Ko', 'Mo', 'Go', 'To');
+
+        return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
+    }
+
 }
